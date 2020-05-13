c7e946da787a83e500b5e40559570c0182c7ee31
Annika Chauhan <annikac0711@gmail.com>
2020-05-13 17:01:40 +0100

    working broadcasts for when one of the users is typing

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const io = socket(server);
 
 io.on('connection', socket => {
     console.log('socket connection made', socket.id);
+    
     socket.on('chat', data => {
         io.sockets.emit('chat', data);
     });
-})
+
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', data);
+    });
+});
--- public/chat.js
+++ public/chat.js
@@ -1,11 +1,11 @@
 // make connection - this is NOT related to the server socket at all, it's it's own entity
-
 const socket = io.connect('http://localhost:3000');
 
 const message = document.getElementById('message');
 const handle = document.getElementById('handle');
 const btn = document.getElementById('send');
 const output = document.getElementById('output');
+const feedback = document.getElementById('feedback');
 
 // emit events on send
 btn.addEventListener('click', () => {
@@ -17,5 +17,14 @@ btn.addEventListener('click', () => {
 
 // listen for events
 socket.on('chat', data => {
+    feedback.innerHTML = '<p></p>';
     output.innerHTML += `<p><strong>${data.handle}</strong>: ${data.message}</p>`;
+});
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing', handle.value);
+});
+
+socket.on('typing', data => {
+    feedback.innerHTML = `<p><em>${data} is typing...</em></p>`;
 });
